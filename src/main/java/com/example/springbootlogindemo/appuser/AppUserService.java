751470e549d7e717aacbf72e10886436af4cf6db
Ozan Halis Ilter <ozanhalisilter@gmail.com>
2022-09-14 23:19:07 +0300

    add User to DB

--- src/main/java/com/example/springbootlogindemo/appuser/AppUserService.java
+++ src/main/java/com/example/springbootlogindemo/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,10 +12,24 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
     private final AppUserRepository appUserRepository;
     private final String USER_NOT_FOUND_MSG = "user with email %s not found";
-
+    private final PasswordEncoder passwordEncoder;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser){
+
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if (userExists){
+            throw new IllegalStateException("user exists");
+        }
+        String encodedPassword = passwordEncoder.encode(appUser.getPassword());
+
+        //TODO Send confirmation token
+        appUser.setPassword(encodedPassword);
+        appUserRepository.save(appUser);
+        return "it worked";
     }
 }
--- src/main/java/com/example/springbootlogindemo/registration/RegistrationService.java
+++ src/main/java/com/example/springbootlogindemo/registration/RegistrationService.java
@@ -1,10 +1,27 @@
 package com.example.springbootlogindemo.registration;
 
+import com.example.springbootlogindemo.appuser.AppUser;
+import com.example.springbootlogindemo.appuser.AppUserRole;
+import com.example.springbootlogindemo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("email not verified");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
